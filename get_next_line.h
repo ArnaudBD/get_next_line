6cbf0cb1ae6c0380cbdfe122a69f4be8927b7ab5
Arnaud BIJU-DUVAL <abiju-du@e2r3p2.42.fr>
2021-04-08 10:57:29 +0200

    ajout du .h a jour

--- get_next_line.h
+++ get_next_line.h
@@ -6,11 +6,18 @@
 # include <sys/types.h>
 # include <sys/stat.h>
 # include <fcntl.h>
+# include <errno.h>
+# include <string.h>
 
 int		get_next_line(int fd, char **line);
+char	*mv_next_line(char **line_dest, char *str);
+int		n_search(char *str);
 void	ft_putnbr_fd(int nb, int fd);
 void	ft_putstr_fd(char *s, int fd);
 char	*ft_strjoin(char const *s1, char const *s2);
 int		ft_strlen(char const *str);
+char	*first_line(char *str);
+char	*cut_first_line(char *str);
+int		mv_first_line(char *str, char **dest);
 
 #endif
